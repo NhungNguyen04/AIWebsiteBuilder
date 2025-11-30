9b282e8c6e3543a1dcfec202dea0ff124f2b9b08
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-11-30 12:02:41 +0700

    Feat: Init Inngest for background jobs handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-import { getQueryClient, trpc } from '@/src/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
-import { Client } from './client';
+"use client"
 
-async function page() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: 'nHUNG' }));
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "../components/ui/button"
+import { useTRPC } from "../trpc/client"
+import { toast } from "sonner";
 
+function page() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job invoked successfully!");
+    }
+  }));
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button disabled={invoke.isPending} onClick={() => {invoke.mutate({text: 'Nhugn'})}}>
+        Invoke Background Job
+      </Button>
+    </div>
   )
 }
 
